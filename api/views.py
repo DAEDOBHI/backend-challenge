e68b96b7cd00cf2141ebbba0e62f1355f6d98bca
Daniel Obregón <daniel.obregon.h@gmail.com>
2020-07-29 10:05:59 -0500

    - add api view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+
+class Products(APIView):
+    def get(self,request):
+        
+        return Response(status=status.HTTP_200_OK)
+
+    def post(self,request):
+        data = request.data
+        try:
+            print(data)
+            return Response(status=status.HTTP_200_OK)
+        except    Exception as err:
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
