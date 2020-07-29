6ae20015e362c9e9f4bf7fbf856209e3da5f25b8
Daniel Obregón <daniel.obregon.h@gmail.com>
2020-07-29 11:15:48 -0500

    -add serializer validations

--- api/models/products.py
+++ api/models/products.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Products(models.Model):
-    id = models.CharField(max_length=20,primary_key=True)
-    name = models.CharField(max_length=250)
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=50)
     value = models.FloatField()
     discount_value = models.FloatField()
     stock = models.IntegerField()
--- api/serializers.py
+++ api/serializers.py
@@ -11,3 +11,18 @@ class ProductSerializer(serializers.ModelSerializer):
                   'value',
                   'discount_value',
                   'stock']
+
+    def validate(self, data):
+        if len(data['name']) < 3 or len(data['name']) > 55:
+            raise serializers.ValidationError(
+                'Invalid product name')
+        if data['value'] < 0 or data['value'] >= 999999.9:
+            raise serializers.ValidationError(
+                'Invalid value')
+        if data['discount_value'] > data['value']:
+            raise serializers.ValidationError(
+                'Invalid discount value')
+        if data['stock'] <= -1:
+            raise serializers.ValidationError(
+                'Invalid stock value')
+        return data
