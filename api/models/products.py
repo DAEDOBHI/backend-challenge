78bea0f2e75e069179a6644a42ffbbb3eb32e3bd
Daniel Obregón <daniel.obregon.h@gmail.com>
2020-07-29 10:27:17 -0500

    - add serializers in the api endpoint, - add project configurations

--- api/models/products.py
+++ api/models/products.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Products(models.Model):
-    product_id = models.CharField(max_length=20)
+    id = models.CharField(max_length=20,primary_key=True)
     name = models.CharField(max_length=250)
     value = models.FloatField()
     discount_value = models.FloatField()
--- api/views.py
+++ api/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from .models import Products
+from .serializers import ProductSerializer
 
 
-class Products(APIView):
-    def get(self,request):
-        
-        return Response(status=status.HTTP_200_OK)
+class ProductsApiView(APIView):
+    def get(self, request):
+        queryset = Products.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self,request):
-        data = request.data
-        try:
-            print(data)
-            return Response(status=status.HTTP_200_OK)
-        except    Exception as err:
-            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(data=data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
